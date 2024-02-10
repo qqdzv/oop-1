df4d0c0a97a0bb9fc32c3607a70582f883ca18d9
adziev khizri <khizri@MacBook-Air-adziev.local>
2024-02-10 12:48:33 +0300

    создание classes.hpp

---
+++ classes.hpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+class Rectangle{
+
+public:
+    
+    Rectangle (float date_x, float date_y) {
+        x = date_x;
+        y = date_y;
+    }
+    int value = 10;
+    void message (){
+        std::cout<<"class Rectangle is working"<<std::endl;;
+    }
+    int get_x(){
+        return x;
+    }
+
+private:
+    float x;
+    float y;
+
+protected:
+   
+};
+
+
